ae574883b901474bef58c926ee87827206bc5721
Guilherme Pola <guilhermemmlonline@outlook.com>
2015-08-13 11:02:04 -0300

    First Commit Project Water Mark in Swift

--- MarcaDagua/ViewController.swift
+++ MarcaDagua/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageViewFoto: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,56 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tirarFoto(sender: UIButton) {
+ 
+        var imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+    }
+    
 
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        
+        
+        var imagemComMarca = adicionarMarcaDagua(image)
+        imageViewFoto.image = imagemComMarca
+        UIImageWriteToSavedPhotosAlbum(imagemComMarca, nil, nil, nil)
+        dismissViewControllerAnimated(true, completion: nil)
+        
+        
+    }
+    
+ 
+    
+    func adicionarMarcaDagua(imagemPrincial: UIImage) -> UIImage{
+    
+        var imagemMarcaDagua = UIImage(named: "waterMark.jpg")
+        
+        UIGraphicsBeginImageContext(imagemPrincial.size)
+        imagemPrincial.drawInRect(CGRect(x: 0, y: 0, width: imagemPrincial.size.width, height: imagemPrincial.size.height))
+        imagemMarcaDagua?.drawInRect(CGRect(x: imagemPrincial.size.width - imagemMarcaDagua!.size.width, y: imagemPrincial.size.height - imagemMarcaDagua!.size.height, width: imagemMarcaDagua!.size.width, height: imagemMarcaDagua!.size.height))
+        
+        let resultadoImagens = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return resultadoImagens
+    
+        
+        
+    }
+    
+
+    @IBAction func longPressImage(sender: UILongPressGestureRecognizer) {
+        
+        let activityViewController = UIActivityViewController(activityItems: [self.imageViewFoto.image!], applicationActivities: nil)
+        
+        self.presentViewController(activityViewController, animated: true, completion: nil)
+        
+    }
+    
 }
 
